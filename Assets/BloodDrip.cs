5d402ac98a7307ce80ab65a18fc4e4662b41d6eb
Ellis Mullen <21334498@studentmail.ul.ie>
2025-09-30 18:25:12 +0100

    BloodDrip Works!
    The change for the blood changing colour finally works and it is adjustable so mess around with it if you want :)
    
    Took me a bit to get it working

--- Assets/BloodDrip.cs
+++ Assets/BloodDrip.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class BloodDrip : MonoBehaviour
 {
-    public GameObject BloodPrefab;     
-    public float dripInterval = 2f;    // seconds between drops
+    public GameObject BloodPrefab;
+    public float dripInterval = 5f;
     public float offsetAboveGround = 0.01f;
+    public float bloodTimer = 10f;
+
+    public Color startTint = Color.white;
+    public Color endTint = new Color(0.7f, 0.4f, 0.3f);
+
     private float dripTimer;
 
-    void Update()
+    void Update() //driptimer is a timer using dripinterval to drop blood on that set interval and resetting
     {
         dripTimer += Time.deltaTime;
 
@@ -18,19 +23,36 @@ public class BloodDrip : MonoBehaviour
         }
     }
 
-    void DropBlood()
+    void DropBlood() //spawn the blood below the player at the first surface it hits then start the coroutine colourchange
     {
+
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 10f))
+        Vector3 origin = transform.position + Vector3.up * 0.1f;
+        if (Physics.Raycast(origin, Vector3.down, out hit, 100f))
         {
-
             Vector3 spawnPos = hit.point + Vector3.up * offsetAboveGround;
-
-            Quaternion rot = Quaternion.Euler(0, Random.Range(0, 360), 0);
-
-            GameObject blood = Instantiate(BloodPrefab, spawnPos, rot);
-            Destroy(blood, 10f);
+            GameObject blood = Instantiate(BloodPrefab, spawnPos, Quaternion.identity);
+            StartCoroutine(ColourChange(blood));
         }
     }
+
+    System.Collections.IEnumerator ColourChange(GameObject blood) //this the blood changing colour, when it gets to the end of the change it destroys itself
+{
+    Renderer rend = blood.GetComponentInChildren<Renderer>();
+
+    float timer = 0f;
+    while (timer < bloodTimer)
+    {
+        timer += Time.deltaTime;
+        float t = timer / bloodTimer;
+
+        rend.material.color = Color.Lerp(startTint, endTint, t);
+
+        yield return null;
+    }
+
+    Destroy(blood);
+}
+
 }
