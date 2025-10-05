91db371c88fa5e681cc85837055499ed6a8ad348
CiaraAMolloy <23376023@studentmail.ul.ie>
2025-10-05 14:32:13 +0100

    added a nav agent and now fishman chases player

--- Assets/Scripts/Fish State Scripts/RandomPatrol.cs
+++ Assets/Scripts/Fish State Scripts/RandomPatrol.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class RandomPatrol : State
 {
@@ -11,22 +13,36 @@ public class RandomPatrol : State
     public GonnaGetYou GGY;
 
     public Logic L;
+    Vector3 goal ;
 
+    private UnityEngine.AI.NavMeshAgent agent;
+    //private Vector3 goal=new Vector3(Random.Range(0,50),Random.Range(0,50),1);
+   
     //run
     public override State RunCurrentState()
     {
+        
+       agent =GameObject.FindGameObjectWithTag("fishman").GetComponent<UnityEngine.AI.NavMeshAgent>();
+       
+       goal= GameObject.FindGameObjectWithTag("Player").transform.position;
+      // Debug.Log(goal);
+        /*
         if (false)
         {//if "sees" player
-            return GGY;  
+            return GGY ;
         }
-         if (L.GetHeardsound())
+        if (L.GetHeardsound())
         {//if hear sound
             return GTS;
         }
-         if (false)
+        if (false)
         { //if find blood
             return BS;
-         }
+        }*/
+        goal= GameObject.FindGameObjectWithTag("Player").transform.position;
+
+
+        agent.SetDestination(goal);
 
 
         return this;
