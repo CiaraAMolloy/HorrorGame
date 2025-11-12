5129b92a4f4ea32fc8f93a31b60c442ce1d45726
CiaraAMolloy <23376023@studentmail.ul.ie>
2025-11-12 11:25:00 +0000

    update ggy
    in case of complete path go to path in case of patail path it goes to the last corner/ part of that path

--- Assets/Scripts/Fish State Scripts/GonnaGetYou.cs
+++ Assets/Scripts/Fish State Scripts/GonnaGetYou.cs
@@ -12,26 +12,49 @@ public class GonnaGetYou : State
 
     public override State RunCurrentState()
     {
-        agent = GameObject.FindGameObjectWithTag("fishman").GetComponent<UnityEngine.AI.NavMeshAgent>();
-         if(l.getChasing())
+    agent = GameObject.FindGameObjectWithTag("fishman").GetComponent<UnityEngine.AI.NavMeshAgent>();
+      var path = new UnityEngine.AI.NavMeshPath();
+ 
+        if(l.getChasing())
         goal = GameObject.FindGameObjectWithTag("Player").transform.position;
+        
         // l.setChasing(true);
 
-        agent.SetDestination(goal);
-       
-        if (!l.getChasing())
-        { //After player gets away/time gone
-          //l.setChasing(false);
-           // if (agent.remainingDistance==0) {
+        
+
+    if (!l.getChasing())
+    { //After player gets away/time gone
+      //l.setChasing(false);
+      // if (agent.remainingDistance==0) {
+      return patrol;
+      // }
+    }
+    else if (l.getisHit())
+    {
+      //    l.setChasing(false);
+      l.hitadressed();
+      return s;
+
+
+    }
+         agent.CalculatePath(goal, path);
+        switch (path.status)
+        {
+            case UnityEngine.AI.NavMeshPathStatus.PathComplete:
+                
+                agent.SetPath(path);
+                break;
+            case UnityEngine.AI.NavMeshPathStatus.PathPartial:
+               
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                goal = path.corners[path.corners.Length - 1];
+                
+                break;
+            default:
                 return patrol;
-           // }
-        }
-        else if(l.getisHit()){
-        //    l.setChasing(false);
-            l.hitadressed();
-            return s;
-
-
+                break;
+              
+                
         }
       
 
